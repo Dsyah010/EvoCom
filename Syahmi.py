81a9af3195b38e663247fa0b46e5e0af451d5163
syahmi danial <94912619+Dsyah010@users.noreply.github.com>
2024-10-29 12:11:37 +0800

    Update Syahmi.py

--- Syahmi.py
+++ Syahmi.py
@@ -1,3 +1,9 @@
+import streamlit as st
+st.set_page_config(
+page_title="Genetic Algorithm"
+)
+st.header("Genetic Algorithm", divider="gray")
+
 import random
 
 #POP_SIZE: Number of Chromosomes in our list.
